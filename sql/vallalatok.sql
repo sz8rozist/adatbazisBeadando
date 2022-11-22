cda4d951ccde3d5b7cdae762dea5d429dc3b1a12
Rózsa István <ristvan98@gmail.com>
2022-11-22 15:53:10 +0100

    sql dump

--- sql/vallalatok.sql
+++ sql/vallalatok.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Nov 21. 18:15
+-- Létrehozás ideje: 2022. Nov 22. 15:51
 -- Kiszolgáló verziója: 10.4.25-MariaDB
 -- PHP verzió: 7.4.30
 
@@ -42,20 +42,16 @@ CREATE TABLE `dolgozik` (
 --
 
 INSERT INTO `dolgozik` (`id`, `projekt_id`, `projekt_nev`, `dolgozo_id`, `kezdes_datum`) VALUES
-(81, 8, 'Havi könyvelés 2022.11', 37, '2022-11-17'),
-(82, 8, 'Havi könyvelés 2022.11', 21, '2022-11-10'),
-(83, 9, 'Havi könyvelés 2022.12', 38, '2022-11-17'),
-(84, 9, 'Havi könyvelés 2022.12', 39, '2022-11-26'),
-(85, 9, 'Havi könyvelés 2022.12', 37, '2022-11-11'),
-(86, 10, 'Havi könyvelés 2022.10', 22, '2022-11-04'),
-(87, 4, 'Bútor Webáruház', 14, '2022-11-12'),
-(88, 4, 'Bútor Webáruház', 15, '2022-11-11'),
-(89, 4, 'Bútor Webáruház', 16, '2022-11-12'),
-(90, 4, 'Bútor Webáruház', 17, '2022-11-26'),
-(91, 4, 'Bútor Webáruház', 27, '2022-11-17'),
-(92, 5, 'CRM Rendszer', 16, '2022-11-18'),
-(93, 5, 'CRM Rendszer', 17, '2022-11-19'),
-(94, 5, 'CRM Rendszer', 28, '2022-11-20');
+(1, 4, 'Rózsakert virág áruház', 1, '2022-11-13'),
+(2, 4, 'Rózsakert virág áruház', 3, '2022-11-12'),
+(3, 4, 'Rózsakert virág áruház', 4, '2022-11-12'),
+(4, 4, 'Rózsakert virág áruház', 7, '2022-11-18'),
+(5, 7, 'Havi könyvelés 2022.10', 8, '2022-11-11'),
+(6, 7, 'Havi könyvelés 2022.10', 9, '2022-11-18'),
+(7, 7, 'Havi könyvelés 2022.10', 24, '2022-11-13'),
+(8, 8, 'Eszközbeszerzés', 10, '2022-11-06'),
+(9, 8, 'Eszközbeszerzés', 22, '2022-11-19'),
+(10, 8, 'Eszközbeszerzés', 23, '2022-12-01');
 
 -- --------------------------------------------------------
 
@@ -80,32 +76,32 @@ CREATE TABLE `dolgozo` (
 --
 
 INSERT INTO `dolgozo` (`id`, `veznev`, `kernev`, `nem`, `szulido`, `fizetes`, `munkakor`, `profilkep`, `osztaly_id`) VALUES
-(14, 'Rózsa', 'István', 1, '1998-02-28', 350000, 'Webfejlesztő', '1669050461_1000_F_230608264_fhoqBuEyiCPwT0h9RtnsuNAId3hWungP.jpg', 13),
-(15, 'Kiss', 'Béla', 1, '2022-11-17', 450000, 'Frontend fejlesztő', NULL, 13),
-(16, 'Lak', 'Zoltán', 1, '2046-01-15', 500000, 'Backend fejlesztő', NULL, 13),
-(17, 'Vicc', 'Elek', 1, '1995-11-11', 600000, 'Backend fejlesztő', '1669050472_man-avatar-profile-vector-21372076.jpg', 13),
-(18, 'Szalmon', 'Ella', 0, '2022-11-17', 360000, 'Projekt Manager', '1669050479_woman-avatar-profile-vector-21372074.jpg', 13),
-(19, 'Trab', 'Antal', 1, '2022-11-17', 450000, 'Projekt Manager', NULL, 15),
-(20, 'Nyomó', 'Réka', 0, '2022-11-25', 580000, 'Projekt Manager', '1669050489_woman-avatar-profile-vector-21372074.jpg', 15),
-(21, 'Major', 'Anna', 0, '2022-11-11', 390000, 'Könyvelő', '1669050497_woman-avatar-profile-vector-21372074.jpg', 16),
-(22, 'Dil', 'Emma', 0, '2022-11-10', 395000, 'Könyvelő', NULL, 16),
-(23, 'Har', 'Mónika', 0, '2001-05-05', 700000, 'Adminisztrátor', NULL, 15),
-(24, 'Rabsz', 'Olga', 0, '1955-04-15', 560000, 'HR vezető', NULL, 14),
-(25, 'Elektrom', 'Ágnes', 0, '1960-11-01', 890000, 'HR vezető', NULL, 14),
-(26, 'Para', 'Zita', 0, '2022-11-09', 456000, 'Tesztelő', NULL, 13),
-(27, 'Git', 'Áron', 1, '2022-11-10', 740000, 'Devops', '1669050514_1000_F_230608264_fhoqBuEyiCPwT0h9RtnsuNAId3hWungP.jpg', 13),
-(28, 'Am', 'Erika', 0, '2022-11-17', 560000, 'Devops', NULL, 13),
-(29, 'Eszte', 'Lenke', 0, '2022-11-07', 250000, 'HR kapcsolattartó', NULL, 14),
-(30, 'Heu', 'Réka', 0, '2022-12-01', 360000, 'HR kapcsolattartó', NULL, 14),
-(31, 'Kala', 'Pál', 1, '1965-02-03', 360000, 'HR kapcsolattartó', '1669050532_man-avatar-profile-vector-21372076.jpg', 14),
-(32, 'Virra', 'Dóra', 0, '2022-11-11', 480000, 'Automatizált Tesztelő', NULL, 13),
-(33, 'Fá', 'Zoltán', 1, '1945-05-06', 450000, 'Automatizált Tesztelő', '1669050542_man-avatar-profile-vector-21372076.jpg', 13),
-(34, 'Wincs', 'Eszter', 0, '1996-07-15', 190000, 'Tesztelő', NULL, 13),
-(35, 'Mor', 'Zsolt', 1, '1963-08-09', 260000, 'Admin', NULL, 15),
-(36, 'Kispál', 'Inka', 1, '1985-09-09', 360000, 'Admin', NULL, 15),
-(37, 'Könyv', 'Elek', 1, '1975-10-15', 560000, 'Könyvelő', NULL, 16),
-(38, 'Koaxk', 'Ábel', 1, '2022-11-01', 750000, 'Vezető Könyvelő', NULL, 16),
-(39, 'Raj', 'Zóra', 0, '2022-11-09', 600000, 'Könyvelő', NULL, 16);
+(1, 'Rózsa', 'István', 1, '1998-02-28', 350000, 'Webfejlesztő', '1669050461_1000_F_230608264_fhoqBuEyiCPwT0h9RtnsuNAId3hWungP.jpg', 1),
+(2, 'Kiss', 'Béla', 1, '2022-11-17', 450000, 'Frontend fejlesztő', NULL, 1),
+(3, 'Lak', 'Zoltán', 1, '2046-01-15', 500000, 'Backend fejlesztő', NULL, 1),
+(4, 'Vicc', 'Elek', 1, '1995-11-11', 600000, 'Backend fejlesztő', '1669050472_man-avatar-profile-vector-21372076.jpg', 1),
+(5, 'Szalmon', 'Ella', 0, '2022-11-17', 360000, 'Projekt Manager', '1669050479_woman-avatar-profile-vector-21372074.jpg', 1),
+(6, 'Trab', 'Antal', 1, '2022-11-17', 450000, 'Projekt Manager', NULL, 1),
+(7, 'Nyomó', 'Réka', 0, '2022-11-25', 580000, 'Projekt Manager', '1669050489_woman-avatar-profile-vector-21372074.jpg', 1),
+(8, 'Major', 'Anna', 0, '2022-11-11', 390000, 'Könyvelő', '1669050497_woman-avatar-profile-vector-21372074.jpg', 4),
+(9, 'Dil', 'Emma', 0, '2022-11-10', 395000, 'Könyvelő', NULL, 4),
+(10, 'Har', 'Mónika', 0, '2001-05-05', 700000, 'Adminisztrátor', NULL, 2),
+(11, 'Rabsz', 'Olga', 0, '1955-04-15', 560000, 'HR vezető', NULL, 3),
+(12, 'Elektrom', 'Ágnes', 0, '1960-11-01', 890000, 'HR vezető', NULL, 3),
+(13, 'Para', 'Zita', 0, '2022-11-09', 456000, 'Tesztelő', NULL, 1),
+(14, 'Git', 'Áron', 1, '2022-11-10', 740000, 'Devops', '1669050514_1000_F_230608264_fhoqBuEyiCPwT0h9RtnsuNAId3hWungP.jpg', 1),
+(15, 'Am', 'Erika', 0, '2022-11-17', 560000, 'Devops', NULL, 1),
+(16, 'Eszte', 'Lenke', 0, '2022-11-07', 250000, 'HR kapcsolattartó', NULL, 3),
+(17, 'Heu', 'Réka', 0, '2022-12-01', 360000, 'HR kapcsolattartó', NULL, 3),
+(18, 'Kala', 'Pál', 1, '1965-02-03', 360000, 'HR kapcsolattartó', '1669050532_man-avatar-profile-vector-21372076.jpg', 3),
+(19, 'Virra', 'Dóra', 0, '2022-11-11', 480000, 'Automatizált Tesztelő', NULL, 1),
+(20, 'Fá', 'Zoltán', 1, '1945-05-06', 450000, 'Automatizált Tesztelő', '1669050542_man-avatar-profile-vector-21372076.jpg', 1),
+(21, 'Wincs', 'Eszter', 0, '1996-07-15', 190000, 'Tesztelő', NULL, 1),
+(22, 'Mor', 'Zsolt', 1, '1963-08-09', 260000, 'Admin', NULL, 2),
+(23, 'Kispál', 'Inka', 1, '1985-09-09', 360000, 'Admin', NULL, 2),
+(24, 'Könyv', 'Elek', 1, '1975-10-15', 560000, 'Könyvelő', NULL, 4),
+(25, 'Koaxk', 'Ábel', 1, '2022-11-01', 750000, 'Vezető Könyvelő', NULL, 4),
+(26, 'Raj', 'Zóra', 0, '2022-11-09', 600000, 'Könyvelő', NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -124,10 +120,10 @@ CREATE TABLE `osztaly` (
 --
 
 INSERT INTO `osztaly` (`id`, `nev`, `manager_id`) VALUES
-(13, 'Szoftverfejlesztő', 14),
-(15, 'Adminisztrátor', 19),
-(14, 'HR', 24),
-(16, 'Könyvelés', 37);
+(1, 'Szoftverfejlesztő', 1),
+(3, 'HR', 2),
+(4, 'Könyvelés', 9),
+(2, 'Adminisztrátor', 22);
 
 -- --------------------------------------------------------
 
@@ -149,17 +145,17 @@ CREATE TABLE `projekt` (
 --
 
 INSERT INTO `projekt` (`id`, `nev`, `ar`, `aktiv`, `osztaly_id`, `osztaly_nev`) VALUES
-(4, 'Bútor Webáruház', 790000, 1, 13, 'Szoftverfejlesztő'),
-(5, 'CRM Rendszer', 1500000, 1, 13, 'Szoftverfejlesztő'),
-(6, 'ERP Rendszer', 2500000, 1, 13, 'Szoftverfejlesztő'),
-(7, 'Rózsakert virág áruház', 600000, 1, 13, 'Szoftverfejlesztő'),
-(8, 'Havi könyvelés 2022.11', 60000, 0, 16, 'Könyvelés'),
-(9, 'Havi könyvelés 2022.12', 150000, 0, 16, 'Könyvelés'),
-(10, 'Havi könyvelés 2022.10', 190000, 1, 16, 'Könyvelés'),
-(11, 'Eszközbeszerzés', 900000, 1, 15, 'Adminisztrátor'),
-(12, 'Műszaki Webáruház', 500000, 0, 13, 'Szoftverfejlesztő'),
-(13, 'Autó Centrum', 500000, 0, 13, 'Szoftverfejlesztő'),
-(14, 'Hálózat fejlesztés', 850000, 0, 15, 'Adminisztrátor');
+(1, 'Bútor Webáruház', 790000, 1, 1, 'Szoftverfejlesztő'),
+(2, 'CRM Rendszer', 1500000, 1, 1, 'Szoftverfejlesztő'),
+(3, 'ERP Rendszer', 2500000, 1, 1, 'Szoftverfejlesztő'),
+(4, 'Rózsakert virág áruház', 600000, 1, 1, 'Szoftverfejlesztő'),
+(5, 'Havi könyvelés 2022.11', 60000, 0, 4, 'Könyvelés'),
+(6, 'Havi könyvelés 2022.12', 150000, 0, 4, 'Könyvelés'),
+(7, 'Havi könyvelés 2022.10', 190000, 1, 4, 'Könyvelés'),
+(8, 'Eszközbeszerzés', 900000, 1, 2, 'Adminisztrátor'),
+(9, 'Műszaki Webáruház', 500000, 0, 1, 'Szoftverfejlesztő'),
+(10, 'Autó Centrum', 500000, 0, 1, 'Szoftverfejlesztő'),
+(11, 'Hálózat fejlesztés', 850000, 0, 2, 'Adminisztrátor');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -193,7 +189,8 @@ ALTER TABLE `osztaly`
 --
 ALTER TABLE `projekt`
   ADD PRIMARY KEY (`id`,`nev`),
-  ADD KEY `osztaly_id` (`osztaly_id`);
+  ADD KEY `osztaly_id` (`osztaly_id`),
+  ADD KEY `osztaly_nev` (`osztaly_nev`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
@@ -203,25 +200,25 @@ ALTER TABLE `projekt`
 -- AUTO_INCREMENT a táblához `dolgozik`
 --
 ALTER TABLE `dolgozik`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT a táblához `dolgozo`
 --
 ALTER TABLE `dolgozo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT a táblához `osztaly`
 --
 ALTER TABLE `osztaly`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT a táblához `projekt`
 --
 ALTER TABLE `projekt`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Megkötések a kiírt táblákhoz
@@ -244,13 +241,13 @@ ALTER TABLE `dolgozo`
 -- Megkötések a táblához `osztaly`
 --
 ALTER TABLE `osztaly`
-  ADD CONSTRAINT `fk_manager_azonosito` FOREIGN KEY (`manager_id`) REFERENCES `dolgozo` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `fk_manager_id` FOREIGN KEY (`manager_id`) REFERENCES `dolgozo` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Megkötések a táblához `projekt`
 --
 ALTER TABLE `projekt`
-  ADD CONSTRAINT `fk_osztaly_id` FOREIGN KEY (`osztaly_id`) REFERENCES `osztaly` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `fk_projekt_osztaly_id` FOREIGN KEY (`osztaly_id`) REFERENCES `osztaly` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
